e6f373615d2e57afc6615b79e8df831d36b42400
Coco29dev <10059@holbertonstudents.com>
2024-12-12 14:14:29 +0000

    code fonctionnel manque betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -3,10 +3,14 @@
 void bubble_sort(int *array, size_t size)
 {
 size_t i, temp;
-size_t flag = 0;
+int global_flag = 0;
+int swap_flag = 0;
 if (size == 0)
 return;
-for (i = 0; i < size; i++)
+while (global_flag == 0)
+{
+swap_flag = 0;
+for (i = 0; i < size - 1; i++)
 {
 if (array[i + 1] < array[i])
 {
@@ -14,9 +18,10 @@ temp = array[i];
 array[i] = array[i + 1];
 array[i + 1] = temp;
 print_array(array, size);
+swap_flag = 1;
 }
-flag = 1;
 }
-if (flag == 1)
-bubble_sort(array, size);
+if (swap_flag != 1)
+global_flag = 1;
+}
 }
