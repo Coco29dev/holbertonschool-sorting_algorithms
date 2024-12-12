6db06c1ef1dcc59cdf60a16a5d0fab88a79104fe
Coco29dev <10059@holbertonstudents.com>
2024-12-12 16:21:38 +0000

    test

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+
+void insertion_sort_list(listint_t **list);
+{
+listint_t *ptr_desc;
+while (*list->next != NULL)
+{
+ptr_desc = list->next;
+if (list->prev && list->n < list->prev->n)
+
+}
