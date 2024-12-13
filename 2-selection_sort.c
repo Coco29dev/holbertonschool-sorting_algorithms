3c106f6f0ade0371dde2b311059ce02ed3edca32
Coco29dev <10059@holbertonstudents.com>
2024-12-13 14:47:59 +0000

    test fonctionnel + betty task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,19 +1,33 @@
 #include "sort.h"
 
+/**
+ * selection_sort - sorts an array of integers in ascending order
+ * @array: array
+ * @size: size
+ * Return: nothing
+ */
+
 
 void selection_sort(int *array, size_t size)
 {
-size_t i, j, temp;
+size_t i, j, temp, min_index;
 if (size == 0)
 return;
-for (i = 0; i < size; i++)
+for (i = 0; i < size - 1; i++)
 {
-for (j = i + 1; j < i; j++)
-if (array[j] > array[i])
+min_index = i;
+for (j = i + 1; j < size; j++)
+{
+if (array[j] < array[min_index])
+{
+min_index = j;
+}
+}
+if (min_index != i)
 {
 temp = array[i];
-array[i] = array[j];
-array[j] = temp;
+array[i] = array[min_index];
+array[min_index] = temp;
 print_array(array, size);
 }
 }
