e800ed60a942179d83d9c3315ac5865909ef08e9
Coco29dev <10059@holbertonstudents.com>
2024-12-13 12:13:11 +0000

    test

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,1 +1,20 @@
 #include "sort.h"
+
+
+void selection_sort(int *array, size_t size)
+{
+size_t i, j, temp;
+if (size == 0)
+return;
+for (i = 0; i < size; i++)
+{
+for (j = i + 1; j < i; j++)
+if (array[j] > array[i])
+{
+temp = array[i];
+array[i] = array[j];
+array[j] = temp;
+print_array(array, size);
+}
+}
+}
